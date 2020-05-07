9c61bac382c2da444cad6b64e705cac70440af03
blastoiselv99 <seanmich@metropolia.fi>
2020-05-08 01:27:43 +0300

    no message

---
+++ Assets/BaseballBat.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseballBat : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _impactPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Enemy")
+        {
+            RichAI rAI = other.GetComponent<RichAI>();
+
+            rAI.HomeRun();
+
+            Instantiate(_impactPrefab, transform.position, transform.rotation);
+        }
+    }
+}

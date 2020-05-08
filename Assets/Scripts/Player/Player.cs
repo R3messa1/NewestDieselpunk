a749741a5467e6d7aef490f62ae3eab00ced5ad0
blastoiselv99 <seanmich@metropolia.fi>
2020-05-08 09:50:55 +0300

    no message

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -91,6 +91,7 @@ public class Player : MonoBehaviour
     public Image damageImage;
     public float flashSpeed = 5f;
     public Color flashColour = new Color(1f, 0f, 0f, 0.1f);
+    public DamageEffect dmgfx;
 
     [SerializeField]
     private DamageEffect _dmgfx;
@@ -379,6 +380,7 @@ public class Player : MonoBehaviour
     public void TakeDamage(float amount)
     {
         _health -= amount;
+        _dmgfx.TakeDamage();
     }
 
     void UpdateHealthBar()
